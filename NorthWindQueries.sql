a8a82f6ea18271016d2b1a4ddb1e83a2d1ca2409
thedigitalmenagerie <honeyraeswan@gmail.com>
2021-09-14 09:11:37 -0400

    third wuery complete

--- NorthWindQueries.sql
+++ NorthWindQueries.sql
@@ -8,14 +8,21 @@ group by OrderID */
 -- 2. What products are currently for sale (not discontinued)? --
 
 
-select ProductName, Products.UnitPrice, Discount, Discontinued
+/* select ProductName, Products.UnitPrice, Discount, Discontinued
 from [Order Details]
 	join [Products]
 		on Products.ProductID = [Order Details].ProductID
-where Discount != 0 and Discontinued != 1
+where Discount != 0 and Discontinued != 1 */
 
 
 -- 3. What is the cost after discount for each order?  Discounts should be applied as a percentage off.--
+
+/* select OrderID,
+	sum(UnitPrice * Quantity) [Undiscounted Subtotal],
+sum((UnitPrice * Quantity) * (1 - Discount)) [Discounted Total]
+from [Order Details]
+group by OrderID */
+
 /* 4. I need a list of sales figures broken down by category name.
 Include the total $ amount sold over all time and the total number of items sold.*/
 -- 5. What are our 10 most expensive products? --

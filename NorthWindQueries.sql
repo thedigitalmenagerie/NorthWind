5631421dd5b1c8256ede9fa2e85e1182d8d5716c
thedigitalmenagerie <honeyraeswan@gmail.com>
2021-09-14 10:00:22 -0400

    sixth query complete

--- NorthWindQueries.sql
+++ NorthWindQueries.sql
@@ -40,4 +40,29 @@ group by UnitPrice, ProductName
 order by UnitPrice DESC */
 
 -- 6. In which quarter in 1997 did we have the most revenue?--
+
+/* select TOP(1)
+	sum(case
+		when month(OrderDate) between 1 and 3 then (UnitPrice * Quantity) * (1 - Discount) else 0 end) [Total Sales Q1 1997],
+	sum(case
+		when month(OrderDate) between 4 and 6 then (UnitPrice * Quantity) * (1 - Discount) else 0 end) [Total Sales Q2 1997],
+	sum(case
+		when month(OrderDate) between 7 and 9 then (UnitPrice * Quantity) * (1 - Discount) else 0 end) [Total Sales Q3 1997],
+	sum(case
+		when month(OrderDate) between 10 and 12 then (UnitPrice * Quantity) * (1 - Discount) else 0 end) [Total Sales Q4 1997]
+from Orders
+	join [Order Details] on Orders.OrderID = [Order Details].OrderID
+where year(OrderDate) = 1997 */
+
+
+/* select TOP(1)
+	Employee.FirstName,
+	Employee.LastName,
+	sum(case when InvoiceDate between '2009-01-01 00:00:00:000' AND '2009-12-31 00:00:00:000' then Total else 0 end) [Total Sales]
+from Invoice
+	join Customer on Customer.CustomerId = Invoice.CustomerId
+	join Employee on Employee.EmployeeId = Customer.SupportRepId
+group by Employee.FirstName, Employee.LastName
+order by [Total Sales] DESC */
+
 -- 7. Which products have a price that is higher than average?--
